859609ca895a7e5cd39260540fd2cbba7f648cf5
Pranav Teegavarapu <46251241+pranavnt@users.noreply.github.com>
2020-12-15 23:11:31 -0800

    lmao

---
+++ content_script.js
@@ -0,0 +1,41 @@
+walk(document.body);
+
+function walk(node) {
+  // I stole this function from here:
+  // http://is.gd/mwZp7E
+
+  var child, next;
+
+  var tagName = node.tagName ? node.tagName.toLowerCase() : "";
+  if (tagName == "input" || tagName == "textarea") {
+    return;
+  }
+  if (node.classList && node.classList.contains("ace_editor")) {
+    return;
+  }
+
+  switch (node.nodeType) {
+    case 1: // Element
+    case 9: // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while (child) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
+}
+
+function handleText(textNode) {
+  var v = textNode.nodeValue;
+
+  v = v.replace(/\oh- \b/g, "hydroxyl");
+
+  textNode.nodeValue = v;
+}
